81a24497fab0ed0d8df085286254748bda20b9f8
pasop18 <jorisnijkamp@gmail.com>
2018-12-14 12:18:26 +0100

    Header en footer toegvoegd aan menu items nog niet de sub menues en connection.php toegevoegd

--- Overons.php
+++ Overons.php
@@ -1,111 +1,37 @@
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AARJO Car BV</title>
-    <link href="css/style.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-</head>
-<body>
-<header class="sectie-main" id="header-main">
-    <div class="sectie-inner">
-        <div class="logo">
-            <a href="index.php">
-                <img src="images/logo.png" alt>
-            </a>
-        </div>
-        <div class="inloggen">
-            <a href="inloggen.php">
-                Inloggen
-            </a>
-        </div>
-    </div>
-    <div class="sectie-inner">
-        <ul id="nav">
-            <li>
-                <a href="index.php">
-                    Home
-                </a>
-            </li>
-            <li>
-                <a href="video.php">
-                    Video's
-                </a>
-                <ul class="submenu">
-                    <li>
-                        <a href="submenu_paginas/motorauto.php">Motor auto</a>
-                    </li>
-                    <li>
-                        <a href="submenu_paginas/componentenauto.php">Componenten auto</a>
-                    </li>
-                    <li>
-                        <a href="submenu_paginas/tipsfeitjes.php">Tips & Feitjes</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="forum.php">
-                    Forum
-                </a>
-            </li>
-            <li>
-                <a href="Overons.php">
-                    Over ons
-                </a>
-            </li>
-        </ul>
-    </div>
-</header>
-<main class="sectie-main">
-    <div class="container-header-foto">
-        <img class="hoofdfoto" src="images/hoofdfoto-3.png" alt>
-        <div class="hoofdfoto-dark-overlay"></div>
-        <div class="schuin-balk-container">
-            <div class="schuin-balk-links"></div>
-            <div class="schuin-balk-rechts"></div>
-        </div>
-    </div>
-    <div class="sectie-inner">
-        <div class="informatie-bouwers">
-            <div class="informatie-jorisaaron">
-                <h1>Joris Nijkamp</h1>
-                <p>
-                    Ik ben Joris Nijkamp. Ik woon in Lochem met mijn ouders en met mijn zus. Ik zit op de HAN in Arnhem op
-                    school en studeer aan HBO-ICT.<br/>
-                    <br/>
-                    Voordat ik begonnen was met HBO-ICT heb ik mbo applicatieontwikkelaar afgerond.<br/>
-                    <br/>
-                    Ik werk bij SiteWork dis is een webdesign bureau en heb het er erg naar me zin.
-                </p>
-                <img src="images/joris.jpg" alt>
-            </div>
-            <div class="informatie-jorisaaron">
-                <h1>Aaron van den Berg</h1>
-                <p>
-                    Ik ben Aaron van den Berg. Ik woon in Geesteren met mijn ouders en met mijn broertje. Ik zit op de HAN in Arnhem op
-                    school en studeer aan HBO-ICT.<br/>
-                    <br/>
-                    Voordat ik begonnen was met HBO-ICT heb ik mbo applicatieontwikkelaar afgerond.<br/>
-                    <br/>
-                    Ik werk bij Remgro dis is een webdesign bureau.
-                </p>
-                <img src="images/aaron.jpg" alt>
+<?php include 'includes/header.php' ?>
+    <main class="sectie-main">
+        <div class="sectie-inner">
+            <div class="informatie-bouwers">
+                <div class="informatie-jorisaaron">
+                    <h1>Joris Nijkamp</h1>
+                    <p>
+                        Ik ben Joris Nijkamp. Ik woon in Lochem met mijn ouders en met mijn zus. Ik zit op de HAN in
+                        Arnhem
+                        op
+                        school en studeer aan HBO-ICT.<br/>
+                        <br/>
+                        Voordat ik begonnen was met HBO-ICT heb ik mbo applicatieontwikkelaar afgerond.<br/>
+                        <br/>
+                        Ik werk bij SiteWork dis is een webdesign bureau en heb het er erg naar me zin.
+                    </p>
+                    <img src="images/joris.jpg" alt>
+                </div>
+                <div class="informatie-jorisaaron">
+                    <h1>Aaron van den Berg</h1>
+                    <p>
+                        Ik ben Aaron van den Berg. Ik woon in Geesteren met mijn ouders en met mijn broertje. Ik zit op
+                        de
+                        HAN in Arnhem op
+                        school en studeer aan HBO-ICT.<br/>
+                        <br/>
+                        Voordat ik begonnen was met HBO-ICT heb ik mbo applicatieontwikkelaar afgerond.<br/>
+                        <br/>
+                        Ik werk bij Remgro dis is een webdesign bureau.
+                    </p>
+                    <img src="images/aaron.jpg" alt>
+                </div>
             </div>
         </div>
-    </div>
 
-</main>
-<footer class="sectie-main">
-    <div class="sectie-inner">
-        <div class="footer-kolom">
-            Gemaakt door - Joris Nijkamp - 20 jaar
-        </div>
-        <div class="footer-kolom">
-            Gemaakt door - Aaron van den Berg - 20 jaar
-        </div>
-    </div>
-</footer>
-</body>
-</html>
+    </main>
+<?php include 'includes/footer.php' ?>
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AARJO Car BV</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+</head>
 <header class="sectie-main" id="header-main">
     <div class="sectie-inner">
         <div class="logo">
