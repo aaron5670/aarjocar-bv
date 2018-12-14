625461fb8e824aa948731af49cd2305bc2915247
pasop18 <jorisnijkamp@gmail.com>
2018-12-14 14:31:15 +0100

    Alle pagina header en footer geincluded en werkt. Behalve de meest diepe pagina's van forum

--- config/config.php
+++ config/config.php
@@ -1,1 +1,5 @@
+<?php
 
+$url = 'http://jorisprojecten.test/aarjocarbvphp/';
+
+?>
--- forum/discussies/het-grote-stel-je-voor-topic.php
+++ forum/discussies/het-grote-stel-je-voor-topic.php
@@ -1,77 +1,9 @@
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AARJO Car BV</title>
-    <link href="../../css/style.css" rel="stylesheet" type="text/css">
-
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="shortcut icon" type="image/x-icon" href="../../favicon.ico">
-</head>
-<body>
-<header class="sectie-main" id="header-main">
-    <div class="sectie-inner">
-        <div class="logo">
-            <a href="../../index.php">
-                <img src="../../images/logo.png" alt>
-            </a>
-        </div>
-        <div class="inloggen">
-            <a href="../../inloggen.php">
-                Inloggen
-            </a>
-        </div>
-    </div>
-    <div class="sectie-inner">
-        <ul id="nav">
-            <li>
-                <a href="../../index.php">
-                    Home
-                </a>
-            </li>
-            <li>
-                <a href="../../video.php">
-                    Video's
-                </a>
-                <ul class="submenu">
-                    <li>
-                        <a href="../../submenu_paginas/motorauto.php">Motor auto</a>
-                    </li>
-                    <li>
-                        <a href="../../submenu_paginas/componentenauto.php">Componenten auto</a>
-                    </li>
-                    <li>
-                        <a href="../../submenu_paginas/tipsfeitjes.php">Tips & Feitjes</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="../../forum.php">
-                    Forum
-                </a>
-            </li>
-            <li>
-                <a href="../../Overons.php">
-                    Over ons
-                </a>
-            </li>
-        </ul>
-    </div>
-</header>
+<?php include '../../includes/header.php' ?>
 <main class="sectie-main">
-    <div class="container-header-foto">
-        <img class="hoofdfoto" src="../../images/hoofdfoto-3.png" alt>
-        <div class="hoofdfoto-dark-overlay"></div>
-        <div class="schuin-balk-container">
-            <div class="schuin-balk-links"></div>
-            <div class="schuin-balk-rechts"></div>
-        </div>
-    </div>
     <div class="sectie-inner">
         <div class="discussie-sectie discussie-sectie-eerste">
             <div class="content-forum-tekst">
-                <h1>SPrijsverschil literprijs benzine vs. diesel</h1>
+                <h1>Prijsverschil literprijs benzine vs. diesel</h1>
                 <p>
                     <strong>Gepost door Joris Nijkamp</strong><br><br>
                 </p>
@@ -79,9 +11,11 @@
             <P>
                 Hallo forumvrienden!
                 <br><br>
-                Ik werd het helemaal zat dat er om de 5 nieuwe topics er minimaal 1 tussenzat waar een nieuw forumlid zich voorstelt.(Ik denk dat ik niet de enige ben)
+                Ik werd het helemaal zat dat er om de 5 nieuwe topics er minimaal 1 tussenzat waar een nieuw forumlid
+                zich voorstelt.(Ik denk dat ik niet de enige ben)
                 <br><br>
-                Dus vandaar dit topic om jezelf eens voor te stellen als nieuw lid, en minimaal 1 foto van je heilige koe
+                Dus vandaar dit topic om jezelf eens voor te stellen als nieuw lid, en minimaal 1 foto van je heilige
+                koe
             </P>
             <div class="content-forum-tekst">
                 <p>
@@ -106,7 +40,8 @@
                 </p>
             </div>
             <P>
-                Goed dat je mijn idee hebt overgenomen.. Er stond in dat topic van uhh, geen idee ook dat 500E het neergelegd heeft bij Jan, maar goed!
+                Goed dat je mijn idee hebt overgenomen.. Er stond in dat topic van uhh, geen idee ook dat 500E het
+                neergelegd heeft bij Jan, maar goed!
             </P>
         </div>
         <div class="discussie-sectie">
@@ -134,15 +69,4 @@
         </div>
     </div>
 </main>
-<footer class="sectie-main">
-    <div class="sectie-inner">
-        <div class="footer-kolom">
-            Gemaakt door - Joris Nijkamp - 20 jaar
-        </div>
-        <div class="footer-kolom">
-            Gemaakt door - Aaron van den Berg - 20 jaar
-        </div>
-    </div>
-</footer>
-</body>
-</html>
+<?php include '../../includes/footer.php' ?>
