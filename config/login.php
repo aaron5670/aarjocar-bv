ed9d7c806ccf2c6acacbeb2dd66c04d1815c56cf
aaron5670 <A2ronda2n>
2018-12-14 15:24:17 +0100

    Fixed login

--- config/login.php
+++ config/login.php
@@ -15,56 +15,55 @@ try {
 	echo "De melding is {$e->getMessage()}";
 }
 
-
 //If the POST var "login" exists (our submit button), then we can
 //assume that the user has submitted the login form.
-if(isset($_POST['login'])){
+if ( isset( $_POST['login'] ) ) {
 
 	//Retrieve the field values from our login form.
-	$username = !empty($_POST['username']) ? trim($_POST['username']) : null;
-	$passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
+	$username        = ! empty( $_POST['username'] ) ? trim( $_POST['username'] ) : null;
+	$passwordAttempt = ! empty( $_POST['password'] ) ? trim( $_POST['password'] ) : null;
 
 	//Retrieve the user account information for the given username.
-	$sql = "SELECT id, username, password FROM users WHERE username = :username";
-	$stmt = $pdo->prepare($sql);
+	$sql  = "SELECT id, username, password FROM users WHERE username = :username";
+	$stmt = $pdo->prepare( $sql );
 
 	//Bind value.
-	$stmt->bindValue(':username', $username);
+	$stmt->bindValue( ':username', $username );
 
 	//Execute.
 	$stmt->execute();
 
 	//Fetch row.
-	$user = $stmt->fetch(PDO::FETCH_ASSOC);
+	$user = $stmt->fetch( PDO::FETCH_ASSOC );
 
-	print_r($user);
+	print_r( $user );
 
 	//If $row is FALSE.
-	if($user === false){
+	if ( $user === false ) {
 		//Could not find a user with that username!
 		//PS: You might want to handle this error in a more user-friendly manner!
-		die('Username not found');
-	} else{
+		die( 'Username not found' );
+	} else {
 		//User account found. Check to see if the given password matches the
 		//password hash that we stored in our users table.
 
 		//Compare the passwords.
-		$validPassword = password_verify($passwordAttempt, $passwordHash);
+		$validPassword = password_verify( $passwordAttempt, $user['password'] );
 
 		//If $validPassword is TRUE, the login has been successful.
-		if($validPassword){
+		if ( $validPassword ) {
 
 			//Provide the user with a login session.
-			$_SESSION['user_id'] = $user['id'];
+			$_SESSION['user_id']   = $user['id'];
 			$_SESSION['logged_in'] = time();
 
 			//Redirect to our protected page, which we called home.php
-			header('Location: index.php');
+			header( 'Location: index.php' );
 			exit;
 
-		} else{
+		} else {
 			//$validPassword was FALSE. Passwords do not match.
-			die('Password do not match');
+			die( 'Password do not match' );
 		}
 	}
 }
