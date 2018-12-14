5ea7358bee2533f941e4714102c0ba51f60f7657
joris Nijkamp <jorisnijkamp@gmail.com>
2018-12-14 11:37:13 +0100

    Site upload

---
+++ aarjocarbv/forum/algemeen.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AARJO Car BV</title>
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">
+</head>
+<body>
+<header class="sectie-main" id="header-main">
+    <div class="sectie-inner">
+        <div class="logo">
+            <a href="../index.php">
+                <img src="../images/logo.png" alt>
+            </a>
+        </div>
+        <div class="inloggen">
+            <a href="../inloggen.php">
+                Inloggen
+            </a>
+        </div>
+    </div>
+    <div class="sectie-inner">
+        <ul id="nav">
+            <li>
+                <a href="../index.php">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="../video.php">
+                    Video's
+                </a>
+                <ul class="submenu">
+                    <li>
+                        <a href="../submenu_paginas/motorauto.php">Motor auto</a>
+                    </li>
+                    <li>
+                        <a href="../submenu_paginas/componentenauto.php">Componenten auto</a>
+                    </li>
+                    <li>
+                        <a href="../submenu_paginas/tipsfeitjes.php">Tips & Feitjes</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="../forum.php">
+                    Forum
+                </a>
+            </li>
+            <li>
+                <a href="../Overons.php">
+                    Over ons
+                </a>
+            </li>
+        </ul>
+    </div>
+</header>
+<main class="sectie-main">
+    <div class="container-header-foto">
+        <img class="hoofdfoto" src="../images/hoofdfoto-3.png" alt>
+        <div class="hoofdfoto-dark-overlay"></div>
+        <div class="schuin-balk-container">
+            <div class="schuin-balk-links"></div>
+            <div class="schuin-balk-rechts"></div>
+        </div>
+    </div>
+    <div class="sectie-inner">
+        <div class="content-forum-tekst">
+            <h1>Algemeen</h1>
+            <p>
+                Auto-gerelateerde zaken die niet in een andere categorie passen.
+            </p>
+        </div>
+
+        <div class="table-container" role="table" aria-label="Destinations">
+            <div class="flex-table header" role="rowgroup">
+                <div class="flex-row first">Onderwerp</div>
+                <div class="flex-row">Bekeken</div>
+                <div class="flex-row">Berichten</div>
+                <div class="flex-row">Laatste</div>
+            </div>
+            <div class="flex-table row" role="rowgroup">
+                <div class="flex-row first">
+                    <a href="discussies/prijsverschil-literprijs-benzine-vs-diesel.php">
+                        <span class="forum-span-title"> Prijsverschil literprijs benzine vs. diesel</span></a><br>
+                    <span class="forum-span-auteur">Door Aaron van den Berg</span>
+                </div>
+                <div class="flex-row">33</div>
+                <div class="flex-row">2</div>
+                <div class="flex-row">18-11-2018 21:05</div>
+            </div>
+            <div class="flex-table row" role="rowgroup">
+                <div class="flex-row first">
+                    <span class="forum-span-title"> Het grote stel je voor topic! </span><br/>
+                    <span class="forum-span-auteur">Door Joris Nijkamp</span>
+                </div>
+                <div class="flex-row">23</div>
+                <div class="flex-row">54</div>
+                <div class="flex-row">18-11-2018 21:05</div>
+            </div>
+            <div class="flex-table row" role="rowgroup">
+                <div class="flex-row first">
+                    <span class="forum-span-title"> Peiling: Wat is rijplezier?</span><br>
+                    <span class="forum-span-auteur">Door Jantje Smit</span>
+                </div>
+                <div class="flex-row">65</div>
+                <div class="flex-row">104</div>
+                <div class="flex-row">18-11-2018 21:05</div>
+            </div>
+            <div class="flex-table row" role="rowgroup">
+                <div class="flex-row first">
+                    <span class="forum-span-title"> HELP EEN AUTOLIEFHEBBENDE STUDENT</span><br>
+                    <span class="forum-span-auteur">Door Kees de Boer</span>
+                </div>
+                <div class="flex-row">39</div>
+                <div class="flex-row">77</div>
+                <div class="flex-row">18-11-2018 21:05</div>
+            </div>
+        </div>
+
+    </div>
+</main>
+<footer class="sectie-main">
+    <div class="sectie-inner">
+        <div class="footer-kolom">
+            Gemaakt door - Joris Nijkamp - 20 jaar
+        </div>
+        <div class="footer-kolom">
+            Gemaatk door - Aaron van den Berg - 20 jaar
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ aarjocarbv/submenu_paginas/tipsfeitjes.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AARJO Car BV</title>
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">
+</head>
+<body>
+<header class="sectie-main" id="header-main">
+    <div class="sectie-inner">
+        <div class="logo">
+            <a href="../index.php">
+                <img src="../images/logo.png" alt>
+            </a>
+        </div>
+        <div class="inloggen">
+            <a href="../inloggen.php">
+                Inloggen
+            </a>
+        </div>
+    </div>
+    <div class="sectie-inner">
+        <ul id="nav">
+            <li>
+                <a href="../index.php">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="../video.php">
+                    Video's
+                </a>
+                <ul class="submenu">
+                    <li>
+                        <a href="motorauto.php">Motor auto</a>
+                    </li>
+                    <li>
+                        <a href="componentenauto.php">Componenten auto</a>
+                    </li>
+                    <li>
+                        <a href="tipsfeitjes.php">Tips & Feitjes</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="../forum.php">
+                    Forum
+                </a>
+            </li>
+            <li>
+                <a href="../Overons.php">
+                    Over ons
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</header>
+<main class="sectie-main">
+    <div class="container-header-foto">
+        <img class="hoofdfoto" src="../images/hoofdfoto-3.png" alt>
+        <div class="hoofdfoto-dark-overlay"></div>
+        <div class="schuin-balk-container">
+            <div class="schuin-balk-links"></div>
+            <div class="schuin-balk-rechts"></div>
+        </div>
+    </div>
+    <div class="sectie-inner">
+        <div class="informatie-videos-container">
+            <div class="informatie-videos">
+                <h1>5 Feitjes</h1>
+                <p>
+                    In deze video zul je 5 feitjes over auto's te horen krijgen om in druk te maken op je vrienden
+                </p>
+                <iframe height="315" src="https://www.youtube-nocookie.com/embed/DxYSz2G6EDk?start=1"
+                        allowfullscreen></iframe>
+            </div>
+            <div class="informatie-videos">
+                <h1>13 Tips om je reis veilig te maken</h1>
+                <p>
+                    In deze video zul je 13 tips te zien krijgen die reis veiliger kunnen maken.
+                </p>
+                <iframe height="315" src="https://www.youtube-nocookie.com/embed/kEtlNKro7Bo?start=1"
+                        allowfullscreen></iframe>
+            </div>
+            <div class="informatie-videos">
+                <h1>10 tips voor beginnende bestuurders</h1>
+                <p>
+                    In deze video zul je 10 tips te zien krijgen voor beginnende bestuurders.
+                </p>
+                <iframe height="315" src="https://www.youtube-nocookie.com/embed/7ClH977D0Yg?start=1"
+                        allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+
+</main>
+<footer class="sectie-main">
+    <div class="sectie-inner">
+        <div class="footer-kolom">
+            Gemaakt door - Joris Nijkamp - 20 jaar
+        </div>
+        <div class="footer-kolom">
+            Gemaakt door - Aaron van den Berg - 20 jaar
+        </div>
+    </div>
+</footer>
+</body>
+</html>
