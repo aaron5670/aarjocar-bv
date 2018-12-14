e3787e9c894412621e5d7732e1e7f73f0099c2b7
aaron5670 <A2ronda2n>
2018-12-14 15:26:35 +0100

    Fixed login

--- config/connect.php
+++ config/connect.php
@@ -1,32 +1,11 @@
 <?php
-//Our MSSQL user account.
-define('MSSQL_USER', 'sa');
-
-//Our MSSQL password.
-define('MSSQL_PASSWORD', 'password1');
-
-//The server that MSSQL is located on.
-define('MSSQL_HOST', 'localhost');
-
-//The name of our database.
-define('MYSQL_DATABASE', 'aarjocar');
-
 /**
- * PDO options / configuration details.
- * I'm going to set the error mode to "Exceptions".
- * I'm also going to turn off emulated prepared statements.
+ * Database connectie
  */
-$pdoOptions = array(
-	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-	PDO::ATTR_EMULATE_PREPARES => false
-);
-
-/**
- * Connect to MySQL and instantiate the PDO object.
- */
-$pdo = new PDO(
-	$db = new PDO( "sqlsrv:Server=localhost;Database=aarjocar", "sa", "password1" ),
-	$pdoOptions //Options
-);
-
-//The PDO object can now be used to query MySQL.
+try {
+	$pdo = new PDO( "sqlsrv:Server=localhost;Database=aarjocar;ConnectionPooling=0", "sa", "password1" );
+	$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+} catch ( PDOException $e ) {
+	echo "Er ging iets mis met de database.<br>";
+	echo "De melding is {$e->getMessage()}";
+}
--- config/login.php
+++ config/login.php
@@ -4,16 +4,8 @@
  */
 session_start();
 
-/**
- * Database connectie
- */
-try {
-	$pdo = new PDO( "sqlsrv:Server=localhost;Database=aarjocar;ConnectionPooling=0", "sa", "password1" );
-	$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-} catch ( PDOException $e ) {
-	echo "Er ging iets mis met de database.<br>";
-	echo "De melding is {$e->getMessage()}";
-}
+//database connectie
+require_once 'connect.php';
 
 //If the POST var "login" exists (our submit button), then we can
 //assume that the user has submitted the login form.
