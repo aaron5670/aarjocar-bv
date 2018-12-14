2e2b2067c40f6c7f97bfecfe3ae0962a61a9c79d
aaron5670 <A2ronda2n>
2018-12-14 11:41:00 +0100

    Changed files

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AARJO Car BV</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+<header class="sectie-main" id="header-main">
+    <div class="sectie-inner">
+        <div class="logo">
+            <a href="index.php">
+                <img src="images/logo.png" alt>
+            </a>
+        </div>
+        <div class="inloggen">
+            <a href="inloggen.php">
+                Inloggen
+            </a>
+        </div>
+    </div>
+    <div class="sectie-inner">
+        <ul id="nav">
+            <li>
+                <a href="index.php">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="video.php">
+                    Video's
+                </a>
+                <ul class="submenu">
+                    <li>
+                        <a href="submenu_paginas/motorauto.php">Motor auto</a>
+                    </li>
+                    <li>
+                        <a href="submenu_paginas/componentenauto.php">Componenten auto</a>
+                    </li>
+                    <li>
+                        <a href="submenu_paginas/tipsfeitjes.php">Tips & Feitjes</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="forum.php">
+                    Forum
+                </a>
+            </li>
+            <li>
+                <a href="Overons.php">
+                    Over ons
+                </a>
+            </li>
+        </ul>
+    </div>
+</header>
+<main class="sectie-main">
+    <div class="container-header-foto">
+        <img class="hoofdfoto" src="images/hoofdfoto-3.png" alt>
+        <div class="hoofdfoto-dark-overlay"></div>
+        <div class="schuin-balk-container">
+            <div class="schuin-balk-links"></div>
+            <div class="schuin-balk-rechts"></div>
+        </div>
+    </div>
+    <div class="sectie-inner">
+        <div class="content-tekst">
+            <h1>Welkom bij AARJO Car BV</h1>
+            <p>
+                Welkom op de website AARJO Car BV. Op deze website kunt u informatie vinden over auto's. U kunt uw
+                kennis over auto's hier uitbreiden. Deze website is bedoeld voor mensen die geïnteresseerd zijn in
+                auto's.
+                <br/>
+                <br/>
+                Ook bevindt zich op deze website een forum. Hier hebben bezoekers de mogelijkheid om met elkaar te
+                discussieren over onderwerpen die betrekken hebben op auto's. Voordat je op dit forum kunt moet een
+                bezoeker zich registrenen en/of inloggen. Dit kunt u doen door rechtsboven op inloggen te
+                klikken. Of door <a href="inloggen.php">hier</a> te klikken.
+                <br/>
+                <br/>
+                Op de video's pagina kunt u een tiental video's bekijken over auto's, dit zullen informatieve video's
+                zijn met een korte beschrijving
+
+
+            </p>
+        </div>
+        <div class="content-forum-items">
+            <h1>Lees hier de meest recente forum berichten!</h1>
+            <div class="forum-item">
+                <h2>Standaard automaat op elke BMW!</h2>
+                <p>
+                    De BMW Steptronic automaat is onmisbaar geworden voor het rijplezier van BMW. Daarom nu
+                    tijdelijk
+                    standaard een automaat op elke BMW.
+                </p>
+            </div>
+            <div class="forum-item">
+                <h2>BMW 5 serie!</h2>
+                <p>
+                    Uw BMW 5 Serie wordt nog aantrekkelijker met de Steptronic automaat. Efficiënter, sportiever en
+                    vooral slimmer. Want dankzij de geavanceerde koppeling met alle andere systemen in uw auto,
+                    schakelt
+                    de Steptronic automatisch naar de settings die optimaal zijn voor uw situatie.
+                </p>
+            </div>
+            <div class="forum-item">
+                <h2>De nieuwe audio Q8!</h2>
+                <p>
+                    De nieuwe Audi Q8 biedt alles waar Audi bekend om staat: een expressief design, toonaangevende
+                    techniek en de kunst om ook de meest veeleisende mensen tevreden te stellen. Zijn sportieve
+                    uiterlijk borduurt voort op de lijnen van de oer-quattro en verwijst met de nieuwe octagonale
+                    Singleframe-grille tevens naar de toekomst.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="sectie-inner">
+        <div class="video-home-container">
+            <iframe class="video-home" height="315"
+                    src="https://www.youtube-nocookie.com/embed/Cw_zy60o8j0?controls=0"
+                    allowfullscreen></iframe>
+
+            <iframe class="video-home" height="315"
+                    src="https://www.youtube-nocookie.com/embed/DrUVMdkb4_k?controls=0"
+                    allowfullscreen></iframe>
+
+            <iframe class="video-home" height="315"
+                    src="https://www.youtube-nocookie.com/embed/lRzIWRLWZgM?controls=0"
+                    allowfullscreen></iframe>
+        </div>
+    </div>
+</main>
+<footer class="sectie-main">
+    <div class="sectie-inner">
+        <div class="footer-kolom">
+            Gemaakt door - Joris Nijkamp - 20 jaar
+        </div>
+        <div class="footer-kolom">
+            Gemaakt door - Aaron van den Berg - 20 jaar
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ inloggen.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AARJO Car BV</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+<header class="sectie-main" id="header-main">
+    <div class="sectie-inner">
+        <div class="logo">
+            <a href="index.php">
+                <img src="images/logo.png" alt>
+            </a>
+        </div>
+        <div class="inloggen">
+            <a href="inloggen.php">
+                Inloggen
+            </a>
+        </div>
+    </div>
+    <div class="sectie-inner">
+        <ul id="nav">
+            <li>
+                <a href="index.php">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="video.php">
+                    Video's
+                </a>
+            </li>
+            <li>
+                <a href="forum.php">
+                    Forum
+                </a>
+            </li>
+            <li>
+                <a href="Overons.php">
+                    Over ons
+                </a>
+            </li>
+        </ul>
+    </div>
+</header>
+<main class="sectie-main">
+    <div class="container-header-foto">
+        <img class="hoofdfoto" src="images/hoofdfoto-3.png" alt>
+        <div class="hoofdfoto-dark-overlay"></div>
+        <div class="schuin-balk-container">
+            <div class="schuin-balk-links"></div>
+            <div class="schuin-balk-rechts"></div>
+        </div>
+    </div>
+    <div class="sectie-inner">
+        <form>
+            <div class="inlog-formulier">
+                <label><b>Gebruikersnaam</b></label>
+                <input type="text" placeholder="Vul hier uw Gebruikersnaam in" name="Gebruikersnaam" required>
+
+                <label><b>Password</b></label>
+                <input type="password" placeholder="Vul hier uw wachtwoord in" name="wachtwoord" required>
+                <label>
+                    Bent u nog niet geregisteerd? Klik dan <a href="registreren.php">hier</a>!
+                </label>
+                <div class="button-container">
+                    <button type="button" class="inlogknop">Inloggen</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+</main>
+<footer class="sectie-main">
+    <div class="sectie-inner">
+        <div class="footer-kolom">
+            Gemaakt door - Joris Nijkamp - 20 jaar
+        </div>
+        <div class="footer-kolom">
+            Gemaakt door - Aaron van den Berg - 20 jaar
+        </div>
+    </div>
+</footer>
+</body>
+</html>
