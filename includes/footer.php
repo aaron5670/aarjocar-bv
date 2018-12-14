623cbdb5c35c0c3e5cd9de8290f6356a8aec58ca
pasop18 <jorisnijkamp@gmail.com>
2018-12-14 11:53:21 +0100

    include header footer

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="sectie-main">
+    <div class="sectie-inner">
+        <div class="footer-kolom">
+            Gemaakt door - Joris Nijkamp - 20 jaar
+        </div>
+        <div class="footer-kolom">
+            Gemaakt door - Aaron van den Berg - 20 jaar
+        </div>
+    </div>
+</footer>
