8a028512c1adb4bfed8da2dfa254f7cc8593c6c7
pasop18 <jorisnijkamp@gmail.com>
2018-12-14 13:30:47 +0100

    links alles werkt behalve sub menus

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AARJO Car BV</title>
-    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <?php if($_SERVER['REQUEST_URI'] == "/aarjocarbvphp/submenu_paginas/componentenauto.php"){
+        echo '  <link href="../css/style.css" rel="stylesheet" type="text/css">';
+    }else{
+        echo '  <link href="css/style.css" rel="stylesheet" type="text/css">';
+    };?>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 </head>
