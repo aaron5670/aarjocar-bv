ac6dd9cdd5df7c5f30fd51cd5a9cb36764a53afa
aaron5670 <A2ronda2n>
2018-12-14 12:15:47 +0100

    updated db file

--- config/db.php
+++ config/db.php
@@ -1,19 +1,19 @@
 <div class="sectie-main">
     <div class="sectie-inner">
         <div class="box-1">
-            <?php include_once 'connection.php' ?>
+			<?php include_once 'connection.php' ?>
         </div>
         <div class="box-2">
-            <?php
-            // select all users
-            $db = new PDO("sqlsrv:Server=localhost;Database=FLETNIX", "sa", "joris123");
+			<?php
+			// select all users
+			$db = new PDO( "sqlsrv:Server=localhost;Database=aarjocar", "sa", "password1" );
 
-            $stmt = $db->query("SELECT * FROM Person");
+			$stmt = $db->query( "SELECT * FROM users" );
 
-            while ($row = $stmt->fetch()) {
-                echo $row['person_id']."\n" . $row['firstname']."\n" . $row['lastname']."\n" . $row['gender']."<br />\n";
-            }
-            ?>
+			while ( $row = $stmt->fetch() ) {
+				echo $row['userID'] . "\n" . $row['username'] . "\n" . $row['password'] . "\n" . "<br />\n";
+			}
+			?>
         </div>
     </div>
 </div>
