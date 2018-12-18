f78ee854ab22577f0b61e7e3111894b36f7d6586
pasop18 <jorisnijkamp@gmail.com>
2018-12-18 13:46:20 +0100

    sql script update

--- SQL/users.sql
+++ SQL/users.sql
@@ -28,3 +28,53 @@ INSERT INTO users VALUES
  ('Joris', 'password')
 
 SELECT * FROM users
+
+
+/* Joris gupdate ff naar kijken aaron denk ik */
+
+
+USE master
+DROP DATABASE aarjocar
+
+CREATE DATABASE aarjocar
+GO
+
+USE aarjocar
+go
+
+CREATE TABLE users (
+	id			int				identity,
+	username	varchar(25)		NOT NULL,
+	password	text			NOT NULL,
+)
+
+CREATE TABLE page_content(
+	pageId		int			identity,
+	titel		varchar(255)	NOT NULL,
+	tekst		varchar(255)	NOT NULL
+)
+
+ALTER TABLE page_content
+ADD CONSTRAINT PK_pageId
+PRIMARY KEY (pageId)
+
+ALTER TABLE users
+ADD CONSTRAINT PK_USERS
+PRIMARY KEY (id)
+
+ALTER TABLE users
+ADD CONSTRAINT UQ_USERS
+UNIQUE (username)
+
+INSERT INTO users VALUES
+ ('Aaron', 'password')
+
+ INSERT INTO users VALUES
+ ('Joris', 'password')
+
+SELECT * FROM users
+
+SELECT * FROM page_content
+
+INSERT INTO page_content values
+	('Welkom bij Aarjocar Bv', 'Test')
