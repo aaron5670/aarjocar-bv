c12d31bac5588be8750c025cb73ecb8e92898632
aaron5670 <A2ronda2n>
2018-12-19 15:26:34 +0100

    Fixed bugs

--- beheerpaneel/video-aanpassen.php
+++ beheerpaneel/video-aanpassen.php
@@ -3,11 +3,15 @@ require_once '../config/connect.php';
 include 'beheer_config/config.php'; ?>
 
 <?php
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-} else if(!isset($_GET['id'])) {
-    echo 'niet gezet';
-    //header('Location: videos.php');
+if ( isset( $_GET['id'] ) ) {
+	$_SESSION['id'] = $_GET['id'];
+	$id             = $_SESSION['id'];
+} else {
+	header( 'location: videos.php' );
+}
+
+if ( isset( $_GET['succes'] ) == true ) {
+	$message = 'Gelukt, alles opgelsagen!';
 }
 ?>
 
@@ -23,24 +27,31 @@ if (isset($_GET['id'])) {
 </head>
 <body>
 <?php include 'menu/menu.php';
-if (isset($_POST['submit'])) {
+if ( isset( $_POST['submit'] ) ) {
 //Haalt de gegevens op van het formulier
-    $iframeurl = !empty($_POST['iframe_url']) ? trim($_POST['iframe_url']) : null;
-    $titel = !empty($_POST['titel']) ? trim($_POST['titel']) : null;
-    $omschrijving = !empty($_POST['omschrijving']) ? trim($_POST['omschrijving']) : null;
-    $kenmerk = !empty($_POST['kenmerk']) ? trim($_POST['kenmerk']) : null;
+	$iframeurl    = ! empty( $_POST['iframe_url'] ) ? trim( $_POST['iframe_url'] ) : null;
+	$titel        = ! empty( $_POST['titel'] ) ? trim( $_POST['titel'] ) : null;
+	$omschrijving = ! empty( $_POST['omschrijving'] ) ? trim( $_POST['omschrijving'] ) : null;
+	$kenmerk      = ! empty( $_POST['kenmerk'] ) ? trim( $_POST['kenmerk'] ) : null;
+	$id           = $_SESSION['id'];
 
 // update page_content query
-    $pdo->query("UPDATE page_iframe SET iframe_url = '$iframeurl', titel = '$titel',
-                                            omschrijving = '$omschrijving', kenmerk = '$kenmerk' WHERE id = '$id'");
+	$pdo->query( "UPDATE page_iframe SET iframe_url = '$iframeurl', titel = '$titel',
+                                            omschrijving = '$omschrijving', kenmerk = '$kenmerk' WHERE id = '$id'" );
 
+	header( 'Location: video-aanpassen.php?id=' . $id . '&succes=true' );
 }
 
 ?>
 <div class="formulier-beheerpaneel-videos">
+	<?php
+	if ( isset( $_GET['succes'] )) {
+		echo '<h1>' . $message . '</h1>';
+	}
+	?>
     <form action="video-aanpassen.php" method="post">
-        <?php $row = $pdo->query("SELECT * FROM page_iframe WHERE id = '$id'");
-        $value = ($row->fetch()); ?>
+		<?php $row = $pdo->query( "SELECT * FROM page_iframe WHERE id = '$id'" );
+		$value     = ( $row->fetch() ); ?>
         <div class="formulier-videos">
             <label for="iframe_url">iframe url</label>
             <input type="text" id="iframe_url" name="iframe_url" value="<?= $value['iframe_url']; ?>"><br>
