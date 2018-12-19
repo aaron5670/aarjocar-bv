a35582848fbabe36022a9cc1fe826863bc9a5278
pasop18 <jorisnijkamp@gmail.com>
2018-12-19 13:29:35 +0100

    home.php beheerpaneel beux fixes enzovoort

--- beheerpaneel/home.php
+++ beheerpaneel/home.php
@@ -11,38 +11,34 @@ require_once '../config/connect.php'; ?>
     <link rel="shortcut icon" type="image/x-icon" href="<?= $url; ?>favicon.ico">
 </head>
 <body>
-<?php include 'menu/menu.php' ?>
+<?php include 'menu/menu.php';
+if (isset($_POST['submit'])) {
 
-<?php
 //Haalt de gegevens op van het formulier
-$titel = !empty($_POST['titel']) ? trim($_POST['titel']) : null;
-$tekst = !empty($_POST['tekst']) ? trim($_POST['tekst']) : null;
-$tekst = base64_encode($tekst);
+    $titel = !empty($_POST['titel']) ? trim($_POST['titel']) : null;
+    $tekst = !empty($_POST['tekst']) ? trim($_POST['tekst']) : null;
+    $tekst = base64_encode($tekst);
 
 // update page_content query
-$pdo->query("UPDATE page_content SET titel = '$titel', tekst = '$tekst' WHERE pageId = 1");
+    $pdo->query("UPDATE page_content SET titel = '$titel', tekst = '$tekst' WHERE pageId = 1");
+}
 
+$row = $pdo->query("SELECT * FROM page_content WHERE pageId = 1");
+$value = ($row->fetch());
 
-$row = $pdo->query("SELECT titel FROM page_content WHERE pageId = 1");
-$valuetitel = ($row->fetch());
-
-$row = $pdo->query("SELECT tekst FROM page_content WHERE pageId = 1");
-$valuetekst = ($row->fetch());
 ?>
 
-<div class="standaard-input-formulier">
-    <div class="formulier-beheerpaneel">
-        <form action="home.php" method="post">
-            <label for="titel">Titel</label>
-            <input type="text" id="titel" name="titel" value="<?= $valuetitel['titel']; ?>"><br>
-            <label for="tekst">Tekst</label>
-            <textarea id="tekst" name="tekst"><?= base64_decode($valuetekst['tekst']); ?></textarea><br>
-            <input type="submit" name="register" value="Register">
-        </form>
-    </div>
-    <div class="formulier-tekst">
 
-    </div>
+<div class="formulier-beheerpaneel">
+    <form action="home.php" method="post">
+        <label for="titel">Titel</label>
+        <input type="text" id="titel" name="titel" value="<?= $value['titel']; ?>"><br>
+
+        <label for="tekst">Tekst</label>
+        <textarea id="tekst" name="tekst"><?= base64_decode($value['tekst']); ?></textarea><br>
+
+        <input type="submit" name="submit" value="Updaten">
+    </form>
 </div>
 </body>
 </html>
--- beheerpaneel/videos.php
+++ beheerpaneel/videos.php
@@ -1,4 +1,5 @@
-<?php include '../config/config.php' ?>
+<?php include '../config/config.php';
+require_once '../config/connect.php'; ?>
 <!DOCTYPE html>
 <html lang="nl">
 <head>
@@ -10,6 +11,49 @@
     <link rel="shortcut icon" type="image/x-icon" href="<?= $url; ?>favicon.ico">
 </head>
 <body>
-<?php include 'menu/menu.php' ?>
+<?php include 'menu/menu.php';
+if (isset($_POST['submit'])) {
+//Haalt de gegevens op van het formulier
+    $iframeurl = !empty($_POST['iframe_url']) ? trim($_POST['iframe_url']) : null;
+    $titel = !empty($_POST['titel']) ? trim($_POST['titel']) : null;
+    $omschrijving = !empty($_POST['omschrijving']) ? trim($_POST['omschrijving']) : null;
+    $kenmerk = !empty($_POST['kenmerk']) ? trim($_POST['kenmerk']) : null;
+
+// update page_content query
+    $pdo->query("UPDATE page_iframe SET iframe_url = '$iframeurl', titel = '$titel',
+                                            omschrijving = '$omschrijving', kenmerk = '$kenmerk'");
+
+}
+
+$row = $pdo->query("SELECT iframe_url FROM page_iframe");
+$valueiframeurl = ($row->fetch());
+
+$row = $pdo->query("SELECT kenmerk FROM page_iframe");
+$valuetitel = ($row->fetch());
+
+$row = $pdo->query("SELECT titel FROM page_iframe");
+$valueomschrijving = ($row->fetch());
+
+$row = $pdo->query("SELECT omschrijving FROM page_iframe");
+$valuekenmerk = ($row->fetch());
+
+?>
+<div class="formulier-beheerpaneel">
+    <form action="videos.php" method="post">
+        <label for="iframe_url">iframe url</label>
+        <input type="text" id="iframe_url" name="iframe_url" value="<?= $valueiframeurl['iframe_url']; ?>"><br>
+
+        <label for="titel">Titel</label>
+        <input type="text" id="titel" name="titel" value="<?= $valuetitel['titel']; ?>"><br>
+
+        <label for="omschrijving">omschrijving</label>
+        <textarea id="omschrijving" name="omschrijving"><?= $valueomschrijving['omschrijving']; ?></textarea><br>
+
+        <label for="kenmerk">kenmerk</label>
+        <input type="text" id="kenmerk" name="kenmerk" value="<?= $valuekenmerk['kenmerk']; ?>"><br>
+
+        <input type="submit" name="submit" value="Updaten">
+    </form>
+</div>
 </body>
 </html>
