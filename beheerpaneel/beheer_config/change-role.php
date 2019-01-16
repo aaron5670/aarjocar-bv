4042e5ff4a3413518ad19eb23129237b9959ec6d
aaron5670 <A2ronda2n>
2019-01-16 15:44:09 +0100

    PDO gemaakt

--- beheerpaneel/beheer_config/change-role.php
+++ beheerpaneel/beheer_config/change-role.php
@@ -4,13 +4,25 @@ require_once 'config.php';
 require_once '../../config/connect.php';
 
 if ( isset( $_GET['makeAdmin'] ) && isset( $_GET['id'] ) ) {
-	$id = $_GET['id'];
-	$pdo->query( "UPDATE users SET user_role = 'admin' WHERE id = $id" );
+	$data = [
+		'id' => $_GET['id']
+	];
+
+	$sql  = "UPDATE users SET user_role = 'admin' WHERE id = :id";
+	$stmt = $pdo->prepare( $sql );
+	$stmt->execute( $data );
+
 	header( 'Location: ' . WEBSITE_URL . 'beheerpaneel/gebruikers.php' );
 }
 if ( isset( $_GET['makeUser'] ) && isset( $_GET['id'] ) ) {
-	$id = $_GET['id'];
-	$pdo->query( "UPDATE users SET user_role = 'user' WHERE id = $id" );
+	$data = [
+		'id' => $_GET['id']
+	];
+
+	$sql  = "UPDATE users SET user_role = 'user' WHERE id = :id";
+	$stmt = $pdo->prepare( $sql );
+	$stmt->execute( $data );
+
 	header( 'Location: ' . WEBSITE_URL . 'beheerpaneel/gebruikers.php' );
 }
 
