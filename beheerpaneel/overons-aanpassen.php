2d4d267c8b278d86fcfed6f4c46dd618aa6a6ee0
aaron5670 <A2ronda2n>
2019-01-16 15:06:15 +0100

    Statements PDO gemaakt!

--- beheerpaneel/overons-aanpassen.php
+++ beheerpaneel/overons-aanpassen.php
@@ -3,15 +3,15 @@ require_once '../config/connect.php';
 include 'beheer_config/config.php'; ?>
 
 <?php
-if (isset($_GET['pageId'])) {
-    $_SESSION['pageId'] = $_GET['pageId'];
-    $id = $_SESSION['pageId'];
+if ( isset( $_GET['pageId'] ) ) {
+	$_SESSION['pageId'] = $_GET['pageId'];
+	$id                 = $_SESSION['pageId'];
 } else {
-    header('location: overons.php');
+	header( 'location: overons.php' );
 }
 
-if (isset($_GET['succes']) == true) {
-    $message = 'Gelukt, alles opgeslagen!';
+if ( isset( $_GET['succes'] ) == true ) {
+	$message = 'Gelukt, alles opgeslagen!';
 }
 ?>
 
@@ -27,38 +27,38 @@ if (isset($_GET['succes']) == true) {
 </head>
 <body>
 <?php include 'menu/menu.php';
-if (isset($_POST['submit'])) {
+if ( isset( $_POST['submit'] ) ) {
 
-//Haalt de gegevens op van het formulier
-    $titel = !empty($_POST['titel']) ? trim($_POST['titel']) : null;
-    $tekst = !empty($_POST['tekst']) ? trim($_POST['tekst']) : null;
-    $id           = $_SESSION['pageId'];
+	//Haalt de gegevens op van het formulier
+	$titel = ! empty( $_POST['titel'] ) ? trim( $_POST['titel'] ) : null;
+	$tekst = ! empty( $_POST['tekst'] ) ? trim( $_POST['tekst'] ) : null;
+	$id    = $_SESSION['pageId'];
 
-    $tekst = base64_encode($tekst);
+	$tekst = base64_encode( $tekst );
 
-// update page_content query
-    $pdo->query("UPDATE page_content SET titel = '$titel', tekst = '$tekst' WHERE pageId = '$id'");
+	// update page_content query
+	$pdo->query( "UPDATE page_content SET titel = '$titel', tekst = '$tekst' WHERE pageId = '$id'" );
 
-    header('Location: overons-aanpassen.php?pageId=' . $id . '&succes=true');
+	header( 'Location: overons-aanpassen.php?pageId=' . $id . '&succes=true' );
 }
 
 ?>
 <div class="formulier-beheerpaneel-videos">
-    <?php
-    if (isset($_GET['succes'])) {
-        echo '<h1>' . $message . '</h1>';
-    }
-    ?>
+	<?php
+	if ( isset( $_GET['succes'] ) ) {
+		echo '<h1>' . $message . '</h1>';
+	}
+	?>
     <form action="overons-aanpassen.php" method="post">
-        <?php $row = $pdo->query("SELECT * FROM page_content WHERE pageId = '$id'");
-        $value = ($row->fetch()); ?>
+		<?php $row = $pdo->query( "SELECT * FROM page_content WHERE pageId = '$id'" );
+		$value     = ( $row->fetch() ); ?>
         <div class="formulier-videos">
 
             <label for="titel">Titel</label>
             <input type="text" id="titel" name="titel" value="<?= $value['titel']; ?>"><br>
 
             <label for="tekst">Tekst</label>
-            <textarea id="tekst" name="tekst"><?= base64_decode($value['tekst']); ?></textarea><br>
+            <textarea id="tekst" name="tekst"><?= base64_decode( $value['tekst'] ); ?></textarea><br>
 
             <input type="submit" name="submit" value="Updaten">
         </div>
