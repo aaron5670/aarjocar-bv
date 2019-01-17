70009fd6a37a6de12e449192416fffbf341ac0c0
pasop18 <jorisnijkamp@gmail.com>
2019-01-17 10:39:54 +0100

    css fix forum

--- forum/algemeen.php
+++ forum/algemeen.php
@@ -10,47 +10,47 @@
 
         <div class="table-container" role="table" aria-label="Destinations">
             <div class="flex-table header" role="rowgroup">
-                <div class="flex-row first">Onderwerp</div>
-                <div class="flex-row">Bekeken</div>
-                <div class="flex-row">Berichten</div>
-                <div class="flex-row">Laatste</div>
+                <div class="flex-row-vervolg-forum first">Onderwerp</div>
+                <div class="flex-row-vervolg-forum">Bekeken</div>
+                <div class="flex-row-vervolg-forum">Berichten</div>
+                <div class="flex-row-vervolg-forum">Laatste</div>
             </div>
             <div class="flex-table row" role="rowgroup">
-                <div class="flex-row first">
+                <div class="flex-row-vervolg-forum first">
                     <a href="discussies/prijsverschil-literprijs-benzine-vs-diesel.php">
                         <span class="forum-span-title"> Prijsverschil literprijs benzine vs. diesel</span></a><br>
                     <span class="forum-span-auteur">Door Aaron van den Berg</span>
                 </div>
-                <div class="flex-row">33</div>
-                <div class="flex-row">2</div>
-                <div class="flex-row">18-11-2018 21:05</div>
+                <div class="flex-row-vervolg-forum">33</div>
+                <div class="flex-row-vervolg-forum">2</div>
+                <div class="flex-row-vervolg-forum">18-11-2018 21:05</div>
             </div>
             <div class="flex-table row" role="rowgroup">
-                <div class="flex-row first">
+                <div class="flex-row-vervolg-forum first">
                     <span class="forum-span-title"> Het grote stel je voor topic! </span><br/>
                     <span class="forum-span-auteur">Door Joris Nijkamp</span>
                 </div>
-                <div class="flex-row">23</div>
-                <div class="flex-row">54</div>
-                <div class="flex-row">18-11-2018 21:05</div>
+                <div class="flex-row-vervolg-forum">23</div>
+                <div class="flex-row-vervolg-forum">54</div>
+                <div class="flex-row-vervolg-forum">18-11-2018 21:05</div>
             </div>
             <div class="flex-table row" role="rowgroup">
-                <div class="flex-row first">
+                <div class="flex-row-vervolg-forum first">
                     <span class="forum-span-title"> Peiling: Wat is rijplezier?</span><br>
                     <span class="forum-span-auteur">Door Jantje Smit</span>
                 </div>
-                <div class="flex-row">65</div>
-                <div class="flex-row">104</div>
-                <div class="flex-row">18-11-2018 21:05</div>
+                <div class="flex-row-vervolg-forum">65</div>
+                <div class="flex-row-vervolg-forum">104</div>
+                <div class="flex-row-vervolg-forum">18-11-2018 21:05</div>
             </div>
             <div class="flex-table row" role="rowgroup">
-                <div class="flex-row first">
+                <div class="flex-row-vervolg-forum first">
                     <span class="forum-span-title"> HELP EEN AUTOLIEFHEBBENDE STUDENT</span><br>
                     <span class="forum-span-auteur">Door Kees de Boer</span>
                 </div>
-                <div class="flex-row">39</div>
-                <div class="flex-row">77</div>
-                <div class="flex-row">18-11-2018 21:05</div>
+                <div class="flex-row-vervolg-forum">39</div>
+                <div class="flex-row-vervolg-forum">77</div>
+                <div class="flex-row-vervolg-forum">18-11-2018 21:05</div>
             </div>
         </div>
 
