205d71f1521458d87950c654925f802b4d3ee157
aaron5670 <A2ronda2n>
2019-01-17 11:11:02 +0100

    Fix for PDO

--- video/video-vervolg.php
+++ video/video-vervolg.php
@@ -1,13 +1,19 @@
 <?php
 require_once '../config/connect.php';
 include '../includes/header.php';
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+if ( isset( $_GET['id'] ) ) {
+	$id = $_GET['id'];
 } else {
-    header('location: index.php');
+	header( 'location: index.php' );
 }
-$stmt = $pdo->query("SELECT * FROM page_iframe");
-$value = $stmt->fetch(); ?>
+$data = [
+	'id' => $id,
+];
+$sql  = 'SELECT * FROM page_iframe WHERE id = :id';
+$stmt = $pdo->prepare( $sql );
+$stmt->execute( $data );
+$value = $stmt->fetch();
+?>
     <div class="sectie-main">
         <div class="sectie-inner">
             <div class="video-vervolg-container">
@@ -19,11 +25,11 @@ $value = $stmt->fetch(); ?>
                 </div>
                 <div class="omschrijving">
                     <h2>Omschrijving video</h2>
-                    <?= $value['omschrijving']; ?>
+					<?= $value['omschrijving']; ?>
                 </div>
                 <div class="publicatiedatum">
                     <h2>Gepubliceerd op:</h2>
-                    <?= $value['publicatiedatum']; ?>
+					<?= $value['publicatiedatum']; ?>
                 </div>
             </div>
         </div>
